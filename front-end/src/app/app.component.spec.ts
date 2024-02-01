3a0c1f836745259bd5e93f999f6aa76a5a116076
mmarci72 <mmarci72@gmail.com>
2024-02-01 14:24:32 +0100

    Fix test for entry point of app

--- front-end/src/app/app.component.spec.ts
+++ front-end/src/app/app.component.spec.ts
@@ -1,32 +1,15 @@
-import { TestBed } from "@angular/core/testing";
-
 import { AppComponent } from "./app.component";
 
 describe("AppComponent", () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [AppComponent],
-    }).compileComponents();
+  let fixture: AppComponent;
+
+  beforeEach(() => {
+    fixture = new AppComponent();
   });
 
-  it("should create the app", () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have the 'front-end' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual("front-end");
-  });
-
-  it("should render title", () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector("h1")?.textContent).toContain(
-      "Hello, front-end"
-    );
+  describe("Setup component", () => {
+    it(`should have the 'Job Hunting Portal' title`, () => {
+      expect(fixture.title).toEqual("Job Hunting Portal");
+    });
   });
 });
