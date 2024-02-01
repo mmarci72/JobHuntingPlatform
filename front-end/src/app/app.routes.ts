c4b4f8e38190c0d3cd5e2e9f9d918523d7984992
mmarci72 <mmarci72@gmail.com>
2024-02-01 15:06:09 +0100

    Add some initial routes

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from "@angular/router";
 
-export const routes: Routes = [];
+import { HomeComponent } from "./home/home.component";
+
+export const routes: Routes = [
+  {
+    path: "",
+    redirectTo: "home",
+    pathMatch: "full",
+  },
+  {
+    path: "home",
+    component: HomeComponent,
+    loadComponent: () =>
+      import("./home/home.component").then(home => home.HomeComponent),
+  },
+];
