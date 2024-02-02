13d5c780209380843ac9b9eded5459b7610857c6
mmarci72 <mmarci72@gmail.com>
2024-02-02 17:42:18 +0100

    Add required position property when testing job cards

--- front-end/src/app/shared/job-card/job-card.component.spec.ts
+++ front-end/src/app/shared/job-card/job-card.component.spec.ts
@@ -1,10 +1,15 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 
+import { Position } from "../../model/job.model";
 import { JobCardComponent } from "./job-card.component";
 
 describe("JobCardComponent", () => {
   let component: JobCardComponent;
   let fixture: ComponentFixture<JobCardComponent>;
+  const testPosition: Position = {
+    id: 10,
+    name: "test",
+  } as Position;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -13,6 +18,7 @@ describe("JobCardComponent", () => {
 
     fixture = TestBed.createComponent(JobCardComponent);
     component = fixture.componentInstance;
+    component.position = testPosition;
     fixture.detectChanges();
   });
 
