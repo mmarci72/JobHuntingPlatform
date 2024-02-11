6408c744e8098a2956bc31b3b6ea44c3f8b340cc
mmarci72 <mmarci72@gmail.com>
2024-02-11 14:59:46 +0100

    Automatically populate positions with the company it belongs to

--- front-end/src/app/service/position.service.ts
+++ front-end/src/app/service/position.service.ts
@@ -1,18 +1,31 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { from, map, mergeMap, toArray } from "rxjs";
 
 import { Position } from "../model/job.model";
 import { BaseService } from "./base.service";
+import { CompanyService } from "./company.service";
 
 @Injectable({
   providedIn: "root",
 })
 export class PositionService extends BaseService<Position> {
-  constructor(http: HttpClient) {
+  constructor(
+    http: HttpClient,
+    private companyService: CompanyService
+  ) {
     super("/positions", http);
   }
 
   public getPositions() {
-    return this.getAllResource();
+    return this.getAllResource().pipe(
+      mergeMap(positions => from(positions)),
+      mergeMap(position => this.populatePositionWithCompany(position)),
+      toArray()
+    );
   }
+  private populatePositionWithCompanyAndLogo = (position: Position) =>
+    this.companyService
+      .getCompanyWithLogos(position.companyId)
+      .pipe(map(company => ({ ...position, company })));
 }
