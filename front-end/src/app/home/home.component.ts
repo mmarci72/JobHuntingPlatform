abaff8ab4e944a0bcf2f20204eeaf5d29765483a
mmarci72 <mmarci72@gmail.com>
2024-03-07 17:46:03 +0100

    Add component for displaying all jobs

--- front-end/src/app/home/home.component.ts
+++ front-end/src/app/home/home.component.ts
@@ -8,9 +8,9 @@ import { MatFormField } from "@angular/material/form-field";
 import { MatInput } from "@angular/material/input";
 import { MatButton } from "@angular/material/button";
 import { NgxPaginationModule } from "ngx-pagination";
-import { Subject, switchMap } from "rxjs";
 import { SearchInputComponent } from "../shared/search-input/search-input.component";
 import { PaginatedPosition } from "../model/job.model";
+import { AllJobsComponent } from "./all-jobs/all-jobs.component";
 
 @Component({
   selector: "app-home",
@@ -26,47 +26,23 @@ import { PaginatedPosition } from "../model/job.model";
     NgxPaginationModule,
     SearchInputComponent,
     AsyncPipe,
+    AllJobsComponent,
   ],
   templateUrl: "./home.component.html",
   styleUrl: "./home.component.scss",
 })
 export class HomeComponent implements AfterViewInit {
   protected readonly PAGE_SIZE = 8;
-  protected current_job_page = 1;
 
   protected newPositions?: PaginatedPosition;
-
-  private filterJobs = new Subject<string>();
-  protected positions$ = this.filterJobs.pipe(
-    switchMap(searchTerm => {
-      return this.positionService.getPositions(
-        this.current_job_page - 1,
-        this.PAGE_SIZE,
-        searchTerm
-      );
-    })
-  );
 
   constructor(private readonly positionService: PositionService) {}
 
   ngAfterViewInit() {
-    let newPositions$ = this.positions$.subscribe(positions => {
-      this.newPositions = positions;
-      newPositions$.unsubscribe();
-    });
-
-    this.filterJobs.next("");
-  }
-
-  protected handlePageChange(event: number) {
-    this.current_job_page = event;
-
-    this.filterJobs.next("");
-  }
-
-  protected onInputChange(text: string) {
-    debugger;
-    this.current_job_page = 1;
-    this.filterJobs.next(text);
+    this.positionService
+      .getPositions(0, this.PAGE_SIZE)
+      .subscribe(positions => {
+        this.newPositions = positions;
+      });
   }
 }
