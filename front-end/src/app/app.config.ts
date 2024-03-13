e9312c61d42574c19f0c048e7621bc7ba6170ac9
mmarci72 <mmarci72@gmail.com>
2024-03-13 17:10:40 +0100

    Add markdown support.

--- front-end/src/app/app.config.ts
+++ front-end/src/app/app.config.ts
@@ -2,9 +2,15 @@ import { provideHttpClient } from "@angular/common/http";
 import { ApplicationConfig } from "@angular/core";
 import { provideAnimations } from "@angular/platform-browser/animations";
 import { provideRouter } from "@angular/router";
+import { provideMarkdown } from "ngx-markdown";
 
 import { routes } from "./app.routes";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient(), provideAnimations()],
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(),
+    provideAnimations(),
+    provideMarkdown(),
+  ],
 };
