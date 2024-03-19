ae49ca5fa3100918e525f8e1ae6ba86bbb4eb269
mmarci72 <mmarci72@gmail.com>
2024-03-19 18:29:45 +0100

    Add snackbar to show status of resume upload

--- front-end/src/app/settings/settings.component.ts
+++ front-end/src/app/settings/settings.component.ts
@@ -1,6 +1,8 @@
 import { Component } from "@angular/core";
 import { MatButton } from "@angular/material/button";
 import { MatCheckbox } from "@angular/material/checkbox";
+import { MatSnackBar } from "@angular/material/snack-bar";
+import { catchError, EMPTY } from "rxjs";
 
 import { AssetService } from "../service/asset.service";
 import { SettingsPreferencesComponent } from "./settings-preferences/settings-preferences.component";
@@ -13,7 +15,13 @@ import { SettingsPreferencesComponent } from "./settings-preferences/settings-pr
   styleUrl: "./settings.component.scss",
 })
 export class SettingsComponent {
-  constructor(private assetService: AssetService) {}
+  private readonly snackbarMessage = "Resume uploaded successfully";
+  private readonly errorSnackbarMessage = "Error uploading the resume";
+
+  constructor(
+    private assetService: AssetService,
+    private snackBar: MatSnackBar
+  ) {}
 
   uploadResume(event: Event) {
     if (!event.target) {
@@ -29,9 +37,18 @@ export class SettingsComponent {
     const file: File = target.files[0];
 
     if (file) {
-      const upload$ = this.assetService.postResume(file);
+      const upload$ = this.assetService.postResume(file).pipe(
+        catchError(() => {
+          this.openSnackBar(this.errorSnackbarMessage);
+          return EMPTY;
+        })
+      );
 
-      upload$.subscribe(() => console.log("FILE UPLOAD SUCCESS"));
+      upload$.subscribe(() => this.openSnackBar(this.snackbarMessage));
     }
+  }
+
+  private openSnackBar(message: string) {
+    this.snackBar.open(message, "OK");
   }
 }
