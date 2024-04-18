1388cbbeee41a9c6792f829235b3c877fbf4d4a6
mmarci72 <mmarci72@gmail.com>
2024-04-18 11:56:27 +0200

    Add markdown-editor and mat-label as dependency

--- front-end/src/app/app.config.ts
+++ front-end/src/app/app.config.ts
@@ -1,23 +1,42 @@
 import {
+  HttpClient,
   provideHttpClient,
   withInterceptorsFromDi,
 } from "@angular/common/http";
-import { ApplicationConfig } from "@angular/core";
+import { ApplicationConfig, importProvidersFrom } from "@angular/core";
 import { provideAnimations } from "@angular/platform-browser/animations";
-import { provideRouter } from "@angular/router";
+import { provideRouter, withComponentInputBinding } from "@angular/router";
+import { AngularMarkdownEditorModule } from "angular-markdown-editor";
 import { KeycloakService } from "keycloak-angular";
-import { provideMarkdown } from "ngx-markdown";
+import { MARKED_OPTIONS, provideMarkdown } from "ngx-markdown";
 
 import { routes } from "./app.routes";
 import { provideKeycloak } from "./auth/keycloak-initializer";
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes),
+    provideRouter(routes, withComponentInputBinding()),
     provideHttpClient(withInterceptorsFromDi()),
     provideAnimations(),
-    provideMarkdown(),
+    provideMarkdown({
+      loader: HttpClient,
+      markedOptions: {
+        provide: MARKED_OPTIONS,
+        useValue: {
+          gfm: true,
+          breaks: false,
+          pedantic: false,
+          smartLists: true,
+          smartypants: false,
+        },
+      },
+    }),
     provideKeycloak(),
     KeycloakService,
+    importProvidersFrom(
+      AngularMarkdownEditorModule.forRoot({
+        iconlibrary: "fa",
+      })
+    ),
   ],
 };
